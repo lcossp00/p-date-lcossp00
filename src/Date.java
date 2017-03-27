7ff5a5bb087b172984de2fdc9335d70238a7ec63
lcossp00 <lcossp00@estudiantes.unileon.es>
2017-03-27 19:11:02 +0200

    pagina3

--- src/Date.java
+++ src/Date.java
@@ -1,24 +1,24 @@
 public class Date{
 	 private int _day, _month, _year;
 	
-public Date(int day, int month, int year){
+public Date(int day, int month, int year){ //Constructor
 	_day = day;
 	_month = month;
 	_year = year;
 }
 	public int getDay(){
-		return this.day;
+		return this._day;
 	}
 	public int getMonth(){
-		return this.month;
+		return this._month;
 	}
 	public int getYear(){
-		return this.year;
+		return this._year;
 	}
 
-	public boolean isSameDay(Date another){
+	public boolean isSameDay(Date another){ //Desplegado para entenderlo mejor
 
-		if(this.day == another.getDay()){
+		if(this._day == another.getDay()){
 			return true;
 		}else{
 			return false; 
@@ -26,24 +26,81 @@ public Date(int day, int month, int year){
 	}
 	public boolean isSameMonth(Date another){
 
-		return (this.month == another.getMonth());
+		return (this._month == another.getMonth());
 	}
 
 	public boolean isSameYear(Date another){
 
-		return (this.year == another.getYear());
+		return (this._year == another.getYear());
+	}
+	public boolean isSame(Date another){
+		if(this._day == another.getDay() && this._month == another.getMonth() && this._year == another.getYear()){
+		return true;
+		}else{
+		return false;
+		}
 	}
 
 
+	public String getNameMonth(){
+		String nameMonth="Enero";
+		switch(this._month){
+			case 1: nameMonth="Enero";
+				break;
+			case 2: nameMonth="Febrero";
+				break;
+			case 3: nameMonth="Marzo";
+				break;
+			case 4:nameMonth="Abril";
+				break;
+			case 5:nameMonth="Mayo";
+				break;
+			case 6:nameMonth="Junio";
+				break;
+			case 7:nameMonth="Julio";
+				break;
+			case 8:nameMonth="Agosto";
+				break;
+			case 9:nameMonth="Septiembre";
+				break;
+			case 10: nameMonth="Octubre";
+				break;
+			case 11: nameMonth="Noviembre";
+				break;
+			case 12:nameMonth="Diciembre";
+				break;
+		}
+		return nameMonth.toString();
+	} 
+	
 
+public int getNumDaysOfMonth(){
+	int numOfDays=0;
+	switch(this._month){
+		case 1: //next
+		case 3: //next
+		case 5: //next
+		case 7: //next
+		case 8: //next
+		case 10: //next
+		case 12: numOfDays=31;
+			break;
+		case 4: //next
+		case 6: //next
+		case 9: //next
+		case 11: numOfDays=30;
+			break;
+		case 2:numOfDays=28;
+			break;
+	}
+	return numOfDays;
+}
 
-
-
-
-
-
-
-
-
-
+public boolean checkDayOfMonth(){
+	if(this._day > this.getNumDaysOfMonth()){
+		return false;
+	}else{
+		return true;
+	}
+}
 }
--- src/MainDate.java
+++ src/MainDate.java
@@ -3,17 +3,22 @@ public class MainDate{
 
 	Date today = new Date (27,3,2017);
 	Date otherDay = new Date (31,3,2017);
-
-	//System.out.println(today.getDay());
-	//System.out.println(otherDay.isSameDay());
-
+	Date today2= new Date (27,3,2017);
+	Date dateFalse= new Date (32,3,2017);
 
 
+	System.out.println(today.getDay());
+	System.out.println(today.isSameDay(otherDay));
+	System.out.println(today.isSame(today2));
 
+	
+	System.out.println(today.getNameMonth());
 
+	System.out.println(today.getNumDaysOfMonth());
 
+	System.out.println("comprobarsi es correcto el numero de dias: "+dateFalse.checkDayOfMonth());
 
-
+	
 
 
 
